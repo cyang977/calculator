0c73cecd0d1bb3258c1793f77c5851777c678317
Chang Yang <cy71997@yahoo.com>
2023-03-18 17:12:10 -0500

    created javascript file

---
+++ script/javascript.js
@@ -0,0 +1,12 @@
+const grid = document.querySelector('.button-area');
+/*createButtons();
+
+
+function createButtons() {
+    for (i = 1; i <= 16; i++){
+        let button = document.createElement('button');
+        button.classList.add('button', `num-${i}`);
+        grid.appendChild(button);
+    }
+    
+}*/ 
