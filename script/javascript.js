01f72d139927bc37d8cbdbda1e8cfa00df692520
Chang Yang <cy71997@yahoo.com>
2023-03-21 15:52:46 -0500

    Finish func for displaying results alongside updates

--- script/javascript.js
+++ script/javascript.js
@@ -1,6 +1,22 @@
-const grid = document.querySelector('.button-area');
+
 const numArray = [];
-testing();
+const previousNum = [];
+const displayArrayValues = [];
+const displayMsgTwo = document.querySelector('.displayMsg-two');
+const displayMsgOne = document.querySelector('.displayMsg-one');
+const buttons = document.querySelectorAll('.click');
+buttons.forEach(button => button.addEventListener('click', () => {
+    console.log(button.textContent);
+    displayInput(button.textContent);
+}));
+const operateButton = document.querySelector('.operate');
+operateButton.addEventListener('click', () => {
+    console.log(displayMsgTwo.textContent);
+    operate(displayMsgTwo.textContent);
+});
+
+
+//testing();
 
 /*function testing(){
     const word = '345+678'
@@ -10,28 +26,63 @@ testing();
     console.log(arrays[1]); 
 }*/
 
+// clears the array for previousNum also updating it. '+' the two numbers together
 function addOperands(a, b) {
-    return a + b;
+    const operandOne = Number(a);
+    const operandTwo = Number(b);
+    previousNum.length = 0;
+    const total = operandOne + operandTwo;
+    previousNum.push(total);
+    numArray.length = 0;
+    displayMsgOne.textContent = `${total}`;
+    displayMsgTwo.textContent = `${total}`;
 }
 
+// clears the array for previousNum also updating it. '-' the two numbers together
 function subtractOperands(a, b) {
-    return a - b;
+    const operandOne = Number(a);
+    const operandTwo = Number(b);
+    previousNum.length = 0;
+    const total = operandOne - operandTwo;
+    previousNum.push(total);
+    numArray.length = 0;
+    displayMsgOne.textContent = `${total}`;
+    displayMsgTwo.textContent = `${total}`;
 }
 
+// clears the array for previousNum also updating it. '*' the two numbers together
 function multiplyOperands(a, b) {
-    return a * b;
+    const operandOne = Number(a);
+    const operandTwo = Number(b);
+    previousNum.length = 0;
+    const total = operandOne * operandTwo;
+    previousNum.push(total);
+    numArray.length = 0;
+    displayMsgOne.textContent = `${total}`;
+    displayMsgTwo.textContent = `${total}`;
 }
 
+// clears the array for previousNum also updating it. '/' the two numbers together
 function divideOperands(a, b) {
-    return a / b;
+    const operandOne = Number(a);
+    const operandTwo = Number(b);
+    previousNum.length = 0;
+    const total = operandOne / operandTwo;
+    previousNum.push(total);
+    numArray.length = 0;
+    displayMsgOne.textContent = `${total}`;
+    displayMsgTwo.textContent = `${total}`;
 }
 
 function operate(expression) {
+    // lets us reset the display box to empty after '=' has been clicked
+    displayArrayValues.length = 0;
     let currentNum;
     if (/[\+]/.test(expression)) {
         currentNum = expression.split('+');
         numArray.push(currentNum[0]);
         numArray.push(currentNum[1]);
+        console.log(numArray);
         currentNum.length = 0;
         addOperands(numArray[0], numArray[1]);
     } else if (/[\-]/.test(expression)) {
@@ -53,6 +104,21 @@ function operate(expression) {
         currentNum.length = 0;
         divideOperands(numArray[0], numArray[1]);
     }
+}
+
+// Adding 'value' arguement into an array, then run next function
+// Uses array as the argument for next function
+function displayInput(value) {
+    displayArrayValues.push(value);
+    displayNums(displayArrayValues);
+}
+
+// Takes an array as an arguement and 'joins' it together to from a string
+// Then uses that string to be displayed with DOM manipulation
+function displayNums(value) {
+    let disVal = value.join('');
+    console.log(disVal);
+    displayMsgTwo.textContent = `${disVal}`;
 }
 
 
