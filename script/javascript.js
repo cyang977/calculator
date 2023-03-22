ef102056ff07a60aba2fc7147212b792704f3128
Chang Yang <cy71997@yahoo.com>
2023-03-22 13:18:20 -0500

    Added functions to allow buttons to change color

--- script/javascript.js
+++ script/javascript.js
@@ -1,25 +1,63 @@
-
+// Created global variables to hold values that will be used later on in the program
+// These variables are datatype arrays
 const numArray = [];
 const previousNum = [];
 const displayArrayValues = [];
 const displayMsgTwo = document.querySelector('.displayMsg-two');
 const displayMsgOne = document.querySelector('.displayMsg-one');
 const buttons = document.querySelectorAll('.click');
+const buttonsOperators = document.querySelectorAll('.operators');
+
+// Adding functionality to operator buttons
+buttonsOperators.forEach(button => button.addEventListener('click', () => {
+    button.classList.add('yellow');
+    displayInput(button);
+}))
+
+// Causes the bg-color to revert back to original based on trasition end
+buttonsOperators.forEach(button => button.addEventListener('transitionend', () => {
+    button.classList.remove('yellow');
+}))
+
+// Adding event listeners to all 'click' class html elements
+// Once clicked, input's values will be added to an array using displayInput() func.
 buttons.forEach(button => button.addEventListener('click', () => {
     console.log(button.textContent);
-    displayInput(button.textContent);
+    button.classList.add('changeButtonColor');
+    displayInput(button);
 }));
+
+// Causes the bg-color to revert back to original based on trasition end
+buttons.forEach(button => button.addEventListener('transitionend', () =>{
+    button.classList.remove('changeButtonColor');
+}))
+
+// Setting the 'click' event to run the 'operate' function
+// This will take in an string expression of what is being displayed   
 const operateButton = document.querySelector('.operate');
 operateButton.addEventListener('click', () => {
+    operateButton.classList.add('lightred');
     console.log(displayMsgTwo.textContent);
     operate(displayMsgTwo.textContent);
 });
 
+// Causes the bg-color to revert back to original based on trasition end
+operateButton.addEventListener('transitionend', () => {
+    operateButton.classList.remove('lightred');
+})
+
+// Once this button is clicked, will clear out displays
+// It sets the array length to 0 and the textContents of both displays to "".
 const acButton = document.querySelector('.ac') 
 acButton.addEventListener('click', () => {
+    acButton.classList.add('blue');
     displayArrayValues.length = 0;
-    displayMsgOne.textContent = "";
-    displayMsgTwo.textContent = "";
+    displayMsgOne.textContent = '';
+    displayMsgTwo.textContent = '';
+})
+
+acButton.addEventListener('transitionend', () => {
+    acButton.classList.remove('blue')
 })
 
 
@@ -57,7 +95,7 @@ function subtractOperands(a, b) {
     displayMsgTwo.textContent = `${total}`;
 }
 
-// clears the array for previousNum also updating it. '*' the two numbers together
+// Clears the array for previousNum also updating it. '*' the two numbers together
 function multiplyOperands(a, b) {
     const operandOne = Number(a);
     const operandTwo = Number(b);
@@ -69,7 +107,7 @@ function multiplyOperands(a, b) {
     displayMsgTwo.textContent = `${total}`;
 }
 
-// clears the array for previousNum also updating it. '/' the two numbers together
+// Clears the array for previousNum also updating it. '/' the two numbers together
 function divideOperands(a, b) {
     const operandOne = Number(a);
     const operandTwo = Number(b);
@@ -81,15 +119,21 @@ function divideOperands(a, b) {
     displayMsgTwo.textContent = `${total}`;
 }
 
+// Used to find if the string expression contains a certain special character
+// Depending on that character, a specific 'if' statement will run
 function operate(expression) {
-    // lets us reset the display box to empty after '=' has been clicked
+    // Lets us reset the display box to empty after '=' has been clicked
     displayArrayValues.length = 0;
     let currentNum;
     if (/[\+]/.test(expression)) {
         currentNum = expression.split('+');
+        // By pushing values into this array, we can then use the values as arguments
+        // These arguments will be used for the calculator funcs
         numArray.push(currentNum[0]);
         numArray.push(currentNum[1]);
         console.log(numArray);
+        // We are clearing the array in currentNum.
+        // That way it will only contain current split array of the new expression
         currentNum.length = 0;
         addOperands(numArray[0], numArray[1]);
     } else if (/[\-]/.test(expression)) {
@@ -116,28 +160,17 @@ function operate(expression) {
 // Adding 'value' arguement into an array, then run next function
 // Uses array as the argument for next function
 function displayInput(value) {
-    displayArrayValues.push(value);
+    const text = value.textContent
+    //value.classList.remove('changeButtonColor');
+    displayArrayValues.push(text);
     displayNums(displayArrayValues);
 }
 
 // Takes an array as an arguement and 'joins' it together to from a string
-// Then uses that string to be displayed with DOM manipulation
+// Then uses that joined array's string to be displayed with DOM manipulation
 function displayNums(value) {
     let disVal = value.join('');
     console.log(disVal);
     displayMsgTwo.textContent = `${disVal}`;
 }
 
-
-
-/*createButtons();
-
-
-function createButtons() {
-    for (i = 1; i <= 16; i++){
-        let button = document.createElement('button');
-        button.classList.add('button', `num-${i}`);
-        grid.appendChild(button);
-    }
-    
-}*/ 
